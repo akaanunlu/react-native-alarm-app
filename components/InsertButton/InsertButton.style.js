f4c67687acf367089d504ce6a0374aea3e5064dc
KAAN Unlu <kaan.unlu@dt.net.tr>
2022-08-18 00:44:32 +0300

    Add insert button component

---
+++ components/InsertButton/InsertButton.style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+	container: {
+		backgroundColor: '#38ddf0',
+		borderRadius: 80,
+		width: 80,
+		height: 80,
+		margin: 10,
+		alignItems: 'center',
+	},
+	text: {
+		color: 'white',
+		fontWeight: '200',
+		margin: 10,
+		fontSize: 50,
+	},
+})
